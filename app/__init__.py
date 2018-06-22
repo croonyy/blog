b3be8c29a34d2153cae488e5523a0a70104f3890
croonyy <38936176+croonyy@users.noreply.github.com>
2018-06-22 13:17:54 +0800

    V0.1

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
+aaaaa
+
+
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-from app import views
 
 # if __name__ == '__main__':
 #    app.run()
