6b8473d894852733d5be1d52a1a31ab1d96450a7
croonyy <38936176+croonyy@users.noreply.github.com>
2018-06-22 14:00:14 +0800

    V0.1

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-aaaaa
-
-
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+# views模块导入为什么放这里而不放头部，
+# 因为app包里面的view模块引用了app变量，如果放头部则app = Flask(__name__)都没有实例化就引用app变量，
+# 所以一定要在app = Flask(__name__)实例化了之后才能引用views模块
+from app import views
 
 # if __name__ == '__main__':
 #    app.run()
